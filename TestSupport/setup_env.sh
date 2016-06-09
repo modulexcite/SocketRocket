6a06d507efde2c3330fff2340c56388e8dea3b7a
Nikita Lutsenko <nlutsenko@me.com>
2016-06-09 16:50:08 -0700

    Make sure wheel, setup tools are installed.

--- TestSupport/setup_env.sh
+++ TestSupport/setup_env.sh
@@ -28,5 +28,9 @@ else
   python $VIRTUALENV_PATH/virtualenv-$VIRTUALENV_VERSION/virtualenv.py $VIRTUALENV_PATH
 
   source $VIRTUALENV_PATH/bin/activate
+  which python
+  
+  pip install wheel
+  pip install setuptools --upgrade
 	pip install autobahntestsuite
 fi
